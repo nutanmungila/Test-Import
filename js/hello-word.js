1a976d2fa5b4d1e5b428433c6619f1f215ce9a09
nutanmungila <nutanmungila@gmail.com>
2015-07-15 12:08:39 +0100

    TestImport

---
+++ js/hello-word.js
@@ -0,0 +1,11 @@
+// hello-world.jsx
+ 
+import React from 'react';
+ 
+class HelloWorld extends React.Component {
+  render() {
+    return (<p>Hello, Nutan!</p>);
+  }
+}
+ 
+React.render(<HelloWorld/>,document.getElementById("div1"));
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from './hello-word';
+ 
+React.render(
+  <HelloWorld />,
+  document.body
+);
